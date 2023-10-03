15fffc58b7dd507944eb557a4c01cad940bea179
CHOI_MIN_GYU <gml8890@naver.com>
2023-10-03 19:27:12 +0900

    ADD - 캘린더 생성 기능, 캘린더 모델링, 기념일 모델링 클래스명 변경, 텍스트 에디터 위젯 생성, API_HOST 변경

--- lib/utils/api_host.dart
+++ lib/utils/api_host.dart
@@ -1,5 +1,5 @@
 class ApiHost {
   // static final API_HOST_DEV = 'http://218.53.23.14:8090';
-  static final API_HOST_DEV = 'http://localhost:8090';
-  // static final API_HOST_DEV = 'http://cherryfeed.shop';
+  // static final API_HOST_DEV = 'http://localhost:8090';
+  static final API_HOST_DEV = 'http://cherryfeed.shop';
 }
